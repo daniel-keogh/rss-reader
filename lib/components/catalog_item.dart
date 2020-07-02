fa18e39a0ee38d366eb8b2b063693cbb56557b42
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2020-07-02 18:30:47 +0100

    Use Stack for CatalogItem

--- lib/components/catalog_item.dart
+++ lib/components/catalog_item.dart
@@ -12,37 +12,51 @@ class CatalogItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      footer: Material(
-        color: Colors.transparent,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            bottom: Radius.circular(4),
+    return Stack(
+      children: <Widget>[
+        Positioned.fill(
+          bottom: 0.0,
+          child: GridTile(
+            footer: Material(
+              color: Colors.transparent,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(4),
+                ),
+              ),
+              clipBehavior: Clip.antiAlias,
+              child: GridTileBar(
+                backgroundColor: Colors.black54,
+                title: FittedBox(
+                  fit: BoxFit.scaleDown,
+                  alignment: AlignmentDirectional.centerStart,
+                  child: Text(photo.title),
+                ),
+              ),
+            ),
+            child: Material(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(4),
+              ),
+              clipBehavior: Clip.antiAlias,
+              child: Image.asset(
+                photo.assetName,
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
         ),
-        clipBehavior: Clip.antiAlias,
-        child: GridTileBar(
-          backgroundColor: Colors.black54,
-          title: FittedBox(
-            fit: BoxFit.scaleDown,
-            alignment: AlignmentDirectional.centerStart,
-            child: Text(photo.title),
+        Positioned.fill(
+          child: Material(
+            color: Colors.transparent,
+            child: InkWell(
+              onTap: () {
+                print(photo.title);
+              },
+            ),
           ),
-        ),
-      ),
-      child: Material(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(4),
-        ),
-        clipBehavior: Clip.antiAlias,
-        child: Ink.image(
-          image: AssetImage(photo.assetName),
-          fit: BoxFit.cover,
-          child: InkWell(
-            onTap: () {},
-          ),
-        ),
-      ),
+        )
+      ],
     );
   }
 }
