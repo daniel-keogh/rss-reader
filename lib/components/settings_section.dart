e50d257b8c353ec412c0ffffae0c322f1e3b05b6
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2020-07-05 19:13:07 +0100

    Create settings_section.dart

---
+++ lib/components/settings_section.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class SettingsSection extends StatelessWidget {
+  final String title;
+  final List<Widget> listTiles;
+
+  SettingsSection({
+    Key key,
+    @required this.title,
+    @required this.listTiles,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.all(16),
+          child: Text(
+            title,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Theme.of(context).accentColor,
+            ),
+          ),
+        ),
+        ListView.separated(
+          separatorBuilder: (context, index) => Divider(),
+          itemBuilder: (context, index) => listTiles[index],
+          itemCount: listTiles.length,
+          physics: NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+        ),
+      ],
+      crossAxisAlignment: CrossAxisAlignment.start,
+    );
+  }
+}
