c22c2c9104f00f6ffad7f28336a9a21228e67073
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2020-07-07 18:45:51 +0100

    Update opml.dart

--- lib/services/opml.dart
+++ lib/services/opml.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
+import 'package:intl/intl.dart';
 import 'package:xml/xml.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -13,27 +14,33 @@ class Opml {
   Future<File> export() async {
     List<Subscription> subscriptions = await _db.getAll();
 
-    Set<String> categories = Set.from(
-      subscriptions.map((item) => item.category),
-    );
-
     final builder = XmlBuilder();
-    builder.processing('xml', 'version="1.0" encoding="utf-8"');
+    builder.processing('xml', 'version="1.0" encoding="UTF-8"');
+
     builder.element('opml', nest: () {
       builder.attribute('version', '2.0');
-      builder.element('head', nest: () {
-        builder.element('title', nest: () {
-          builder.text('rssfeed export');
-        });
-        builder.element('dateCreated', nest: () {
-          builder.text(DateTime.now());
-        });
-      });
+
+      builder.element(
+        'head',
+        isSelfClosing: false,
+        nest: () {
+          builder.element('title', nest: () {
+            builder.text('rssfeed Export');
+          });
+          builder.element('dateCreated', nest: () {
+            builder.text(DateTime.now().toUtc().toIso8601String());
+          });
+        },
+      );
 
       builder.element(
         'body',
         isSelfClosing: false,
         nest: () {
+          Set<String> categories = Set.from(
+            subscriptions.map((item) => item.category),
+          );
+
           categories.forEach((category) {
             builder.element('outline', nest: () {
               builder.attribute('text', category);
@@ -42,9 +49,9 @@ class Opml {
               subscriptions.forEach((sub) {
                 if (sub.category == category) {
                   builder.element('outline', nest: () {
+                    builder.attribute('type', 'rss');
                     builder.attribute('text', sub.title);
                     builder.attribute('title', sub.title);
-                    builder.attribute('type', 'rss');
                     builder.attribute('xmlUrl', sub.xmlUrl);
                   });
                 }
@@ -56,12 +63,16 @@ class Opml {
     });
 
     final xml = builder.build();
-    return await _writeXmlString(xml);
+    return await _writeXmlToFile(xml);
   }
 
-  Future<File> _writeXmlString(XmlNode xml) async {
+  Future<File> _writeXmlToFile(XmlNode xml) async {
     final directory = await getExternalStorageDirectory();
-    final file = File('${directory.path}/subscriptions.opml');
+
+    final df = DateFormat('yyyy-MM-dd');
+    final file = File(
+      '${directory.path}/rssreader_${df.format(DateTime.now())}.opml',
+    );
 
     return file.writeAsString(
       xml.toXmlString(
@@ -71,10 +82,10 @@ class Opml {
     );
   }
 
-  Future import() async {
+  Future<void> import() async {
     File file = await FilePicker.getFile(
       type: FileType.custom,
-      allowedExtensions: ['opml', 'xml'],
+      allowedExtensions: ['bin', 'xml'],
     );
 
     try {
@@ -97,13 +108,20 @@ class Opml {
             (attr) => attributes[attr.name.toString()] = attr.value,
           );
 
-          await _db.insert(
-            Subscription(
-              title: attributes['title'],
-              xmlUrl: attributes['xmlUrl'],
-              category: category?.value ?? 'Uncategorized',
-            ),
-          );
+          if (attributes['type'] == 'rss') {
+            try {
+              await _db.insert(
+                Subscription(
+                  title: attributes['text'] ?? attributes['title'],
+                  category: category?.value ?? 'Uncategorized',
+                  xmlUrl: attributes['xmlUrl'],
+                ),
+              );
+            } catch (e) {
+              // TODO: Handle SQL exception
+              print(e);
+            }
+          }
         });
       });
     } catch (e) {
