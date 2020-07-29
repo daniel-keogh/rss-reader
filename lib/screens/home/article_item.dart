6cd8681cdf14be49c2baf1d405d4617fdf4b5dbe
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2020-07-29 19:34:11 +0100

    Article item improvements

--- lib/screens/home/article_item.dart
+++ lib/screens/home/article_item.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+
+import 'package:cached_network_image/cached_network_image.dart';
 
 import 'package:rssreader/models/article.dart';
 
@@ -16,26 +20,50 @@ class ArticleItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Padding(
-        padding: EdgeInsets.only(bottom: 6.0),
-        child: Text(
-          article.title,
-          style: TextStyle(
-            fontSize: 15,
-            fontWeight: FontWeight.w500,
-            color: article.isRead ? Colors.grey : null,
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        ListTile(
+          title: Padding(
+            padding: EdgeInsets.only(bottom: 10.0),
+            child: Text(
+              article.title,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+                color: article.isRead ? Colors.grey : null,
+              ),
+            ),
           ),
+          subtitle: Text(
+            article.publisher,
+            style: TextStyle(fontSize: 12),
+          ),
+          trailing: article.imageUrl != null
+              ? ClipRRect(
+                  borderRadius: const BorderRadius.all(
+                    const Radius.circular(4),
+                  ),
+                  child: SizedBox(
+                    width: 100.0,
+                    child: CachedNetworkImage(
+                      imageUrl: article.imageUrl,
+                      fit: BoxFit.cover,
+                      fadeInDuration: const Duration(milliseconds: 250),
+                      errorWidget: (context, url, error) => Container(
+                        child: const Center(
+                          child: const Icon(Icons.error),
+                        ),
+                        color: Colors.black.withOpacity(0.2),
+                      ),
+                    ),
+                  ),
+                )
+              : null,
+          onTap: handleTap,
+          onLongPress: handleLongPress,
         ),
-      ),
-      subtitle: Text(
-        article.publisher,
-        style: TextStyle(fontSize: 12),
-      ),
-      leading:
-          article.imageUrl != null ? Image.network(article.imageUrl) : null,
-      onTap: handleTap,
-      onLongPress: handleLongPress,
+      ],
     );
   }
 }
